59c4bc682dfd86adc5715fcc0651097c00bd4340
Bulat <bulat067@gmail.com>
2025-07-11 00:18:17 +0300

    Fix image

--- frontend/src/components/custom/HeroSection.tsx
+++ frontend/src/components/custom/HeroSection.tsx
@@ -27,12 +27,14 @@ export function HeroSection({ data }: Readonly<HeroSectionProps>) {
   console.dir(data, { depth: null });
   const { heading, subHeading, image, link } = data;
   console.log(image);
-  const imageURL = "http://localhost:1337" + image.url;
+  const STRAPI_URL =
+    process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+  const imageURL = image?.url ? STRAPI_URL + image.url : "";
   return (
     <header className="relative h-[600px] overflow-hidden">
       <img
         alt="Background"
-        className="absolute inset-0 object-cover w-full h-full"
+        className="absolute inset-0 object-cover w-full h-full z-0"
         height={1080}
         src={imageURL}
         style={{
@@ -41,17 +43,19 @@ export function HeroSection({ data }: Readonly<HeroSectionProps>) {
         }}
         width={1920}
       />
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black/50 px-4">
         <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl">
           {heading}
         </h1>
         <p className="mt-4 text-lg md:text-xl lg:text-2xl">{subHeading}</p>
-        <Link
-          className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
-          href={link.url}
-        >
-          {link.text}
-        </Link>
+        {link?.url && link?.text && (
+          <Link
+            className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
+            href={link.url}
+          >
+            {link.text}
+          </Link>
+        )}
       </div>
     </header>
   );
