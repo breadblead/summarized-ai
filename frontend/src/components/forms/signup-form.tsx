8621ac9c7e9c5b83e0ee3d2784ad251e22017f81
Bulat <bulat067@gmail.com>
2025-07-27 18:59:21 +0300

    Form Validation with Zod

--- frontend/src/components/forms/signup-form.tsx
+++ frontend/src/components/forms/signup-form.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { registerUserAction } from "@/data/actions/auth-actions";
 import { useActionState } from "react";
+import { registerUserAction } from "@/data/actions/auth-actions";
 
 import {
   CardTitle,
@@ -16,6 +16,8 @@ import {
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
+import { ZodErrors } from "@/components/custom/zod-errors";
+
 const INITIAL_STATE = {
   data: null,
 };
@@ -25,9 +27,10 @@ export function SignupForm() {
     registerUserAction,
     INITIAL_STATE
   );
+
   console.log("## will render on client ##");
   console.log(formState);
-  console.log("#########################");
+  console.log("###########################");
 
   return (
     <div className="w-full max-w-md">
@@ -48,6 +51,7 @@ export function SignupForm() {
                 type="text"
                 placeholder="username"
               />
+              <ZodErrors error={formState?.zodErrors?.username} />
             </div>
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
@@ -57,6 +61,7 @@ export function SignupForm() {
                 type="email"
                 placeholder="name@example.com"
               />
+              <ZodErrors error={formState?.zodErrors?.email} />
             </div>
 
             <div className="space-y-2">
@@ -67,6 +72,7 @@ export function SignupForm() {
                 type="password"
                 placeholder="password"
               />
+              <ZodErrors error={formState?.zodErrors?.password} />
             </div>
           </CardContent>
           <CardFooter className="flex flex-col">
--- frontend/src/data/actions/auth-actions.ts
+++ frontend/src/data/actions/auth-actions.ts
@@ -1,18 +1,43 @@
 "use server";
 
+import { z } from "zod";
+
+const schemaRegister = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters" })
+    .max(20, { message: "Username must be no more than 20 characters" }),
+
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(100, { message: "Password must be no more than 100 characters" }),
+
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+});
+
 export async function registerUserAction(prevState: any, formData: FormData) {
   console.log("Hello From Register User Action");
 
-  const fields = {
+  const validatedFields = schemaRegister.safeParse({
     username: formData.get("username"),
     password: formData.get("password"),
     email: formData.get("email"),
-  };
+  });
 
-  console.log(fields);
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      strapiErrors: null,
+      message: "Missing Fields. Failed to Register.",
+    };
+  }
 
   return {
     ...prevState,
-    data: fields,
+    data: "ok",
   };
 }
